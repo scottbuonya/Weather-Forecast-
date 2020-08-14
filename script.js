7c6b74a982a45d5caa0db7a653abf545f19cb3e2
Scott Buonya <scottbuonya@gmail.com>
2020-08-14 17:54:18 -0400

    uv color index

--- script.js
+++ script.js
@@ -38,6 +38,37 @@ $(document).ready(function() {
         buildFiveDayForecast();
     };
 
+   //current weather function
+    function buildTodaysWeather () {
+        var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity +"&appid=" + APIKey;
+
+        $.ajax({
+            url: currentWeatherURL,
+            method: "GET"
+          }).then(function(todaysWeather) {
+
+            
+            $("#todays-weather").empty();
+           
+            var newDiv = $("<div>").addClass("card-body");
+            
+            var newH4 = $("<h4>",{class: "card-title", text: inputCity + " (Current) "});  
+            
+            var icon =todaysWeather.weather[0].icon;
+            var iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            var newI = $("<img>").attr("src", iconURL);            
+            newH4.append(newI);
+          
+             //temp, converted from kelvin
+             var tempFromKelvin = (todaysWeather.main.temp - 273.15) * 1.80 + 32
+             var newP1 = $("<p>",{class: "card-text", text: "Temperature: " + tempFromKelvin.toFixed(1) + " °F"}); 
+             
+             var newP2 = $("<p>",{class: "card-text", text: "Humidity: " + todaysWeather.main.humidity +"%"});
+             
+             var newP3 = $("<p>",{class: "card-text", text: "Wind Speed: " + todaysWeather.wind.speed + " MPH"});                
+             
+             var newP4 = $("<p>",{class: "card-text", text: "UV Index: "});
+
 
 
 
