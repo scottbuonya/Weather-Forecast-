6a274924b4a0fb0a409e2a0eef389f80fc46f97f
Scott Buonya <scottbuonya@gmail.com>
2020-08-14 18:02:20 -0400

    function for building the forecast

--- script.js
+++ script.js
@@ -68,6 +68,75 @@ $(document).ready(function() {
              var newP3 = $("<p>",{class: "card-text", text: "Wind Speed: " + todaysWeather.wind.speed + " MPH"});                
              
              var newP4 = $("<p>",{class: "card-text", text: "UV Index: "});
+          
+
+             var latValue = todaysWeather.coord.lat;
+             var lonValue = todaysWeather.coord.lon;
+ 
+             var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latValue + "&lon=" + lonValue;
+
+
+             $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function(uvWeather) {
+                
+                var uvValue = uvWeather.value;
+                
+                //get the uv colors based on the uv index
+                var uvColor = "";
+                if (uvValue < 3){
+                    uvColor = "lowuv"
+                }
+                else if (uvValue < 6){
+                    uvColor = "mediumuv"                    
+                }
+                else if (uvValue < 8){
+                    uvColor = "highuv"                    
+                }                
+                else if (uvValue < 11){
+                    uvColor = "veryhighuv"                    
+                }                  
+                else {
+                    uvColor = "extremelyhighuv"                    
+                };
+
+                var newSpan = $("<span>",{class: uvColor, text: uvValue});                        
+                newP4.append(newSpan);
+                newDiv.append(newH4, newP1, newP2, newP3, newP4);
+                $("#todays-weather").append(newDiv);
+            });
+        });
+
+
+    }
+
+    //function builds the five day forecast
+    
+    function buildFiveDayForecast () {
+
+        var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&appid=" + APIKey;
+        
+
+        // builds 5 different cards using forecast
+        $.ajax({
+            url: fiveDayURL,
+            method: "GET"
+          }).then(function(fiveDaysWeather) {
+            console.log(fiveDaysWeather);
+            $("#fivedaywords").empty();
+            $("#fivedaysection").empty();        
+    
+            $("#fivedaywords").text("5-Day Forecast");
+
+            
+            var element3PMFirstAppears = 0;
+            for (i = 0; i < 8; i++) {
+                if (fiveDaysWeather.list[i].dt_txt.includes("15:00:00")) {
+                    element3PMFirstAppears = i;
+                    break;
+                }
+            }
 
 
 
