eddcc247ca434c4fd98d107e86a17d973856738c
Scott Buonya <scottbuonya@gmail.com>
2020-09-10 16:07:11 -0400

    added my html structure, css, and added search bar to city names to appear in list

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,16 @@
+$(document).ready(function(){
+    $("#button-addon2").on("click", function(){
+    var cityName = $("#citySearch").val()
+    console.log(cityName)
+    var cityList = $(".list-group")
+    var newCity = $("<li class='list-group-item'></li>").text(cityName)
+    cityList.prepend(newCity)
+    })
+})
 
-
+//grabbing the ul 
+//create a new list item with city name inside
+//prepend new list item into ul
 
 
 
